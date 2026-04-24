071cfd013196f01c03786edfa4f9fed60cb58d2e
mouad <mouadzed1@gmail.com>
2026-04-24 09:10:40 +0100

    add get the bookingtermine to get the booking that conforimed and the end_time pass

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -13,61 +13,79 @@ class ClientController extends Controller
 {
     public function dashboard()
     {
-       
+
         $services = Service::all();
-        return view('client.dashboard', 
-        compact('services'));
+        return view(
+            'client.dashboard',
+            compact('services')
+        );
 
     }
-    public function mybooking(){
-        $booking = booking::where('user_id',Auth::user()->id)->get();
-        // dd($booking);
-        return view('client.barbers', 
-        compact('booking'));
+    public function mybooking()
+    {
+        $bookings = booking::where('user_id', Auth::user()->id)
+            ->where('status', 'pending')
+            ->get();
+        $bookingTermine = Booking::with('timeSlot')
+            ->where('user_id', Auth::id())
+            ->where('status', 'confirmed')
+            ->whereRelation(
+                'timeSlot',
+                'end_time',
+                '<',
+                now()->format('H:i')
+            )
+            ->get();
+        // dd($bookingTermine);
+        return view(
+            'client.mybooking',
+            compact('bookings','bookingTermine')
+        );
     }
-    public function profile(){
-        $bookings  =Booking::all();
-        return view("client.profile",compact('bookings'));
+    public function profile()
+    {
+
+        return view("client.profile");
     }
-   public function updateProfile(Request $request)
-{
-    $user = Auth::user();
+    public function updateProfile(Request $request)
+    {
+        $user = Auth::user();
 
-    $data = $request->validate([
-        'ferstname' => 'required|string|max:191',
-        'lastname' => 'required|string|max:191',
-        'email' => 'required|email|unique:users,email,' . $user->id,
-        'telephone' => 'required|string',
-        'password' => 'nullable|min:4|confirmed'
-    ]);
+        $data = $request->validate([
+            'ferstname' => 'required|string|max:191',
+            'lastname' => 'required|string|max:191',
+            'email' => 'required|email|unique:users,email,' . $user->id,
+            'telephone' => 'required|string',
+            'password' => 'nullable|min:4|confirmed'
+        ]);
 
-    
-    $user->ferstname = $data['ferstname'];
-    $user->lastname = $data['lastname'];
-    $user->email = $data['email'];
-    $user->telephone = $data['telephone'];
 
-    
-    if (!empty($data['password'])) {
-        $user->password = Hash::make($data['password']);
+        $user->ferstname = $data['ferstname'];
+        $user->lastname = $data['lastname'];
+        $user->email = $data['email'];
+        $user->telephone = $data['telephone'];
+
+
+        if (!empty($data['password'])) {
+            $user->password = Hash::make($data['password']);
+        }
+        //   dd($data);
+        $user->save();
+
+        return back()->with('success', 'Profil mis à jour ✅');
     }
-    //   dd($data);
-    $user->save();
+    public function destroy(Request $request)
+    {
+        $user = Auth::user();
 
-    return back()->with('success', 'Profil mis à jour ✅');
-}
-public function destroy(Request $request)
-{
-    $user = Auth::user();
+        Auth::logout();
 
-    Auth::logout();
+        $user->delete();
 
-    $user->delete();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
-    $request->session()->invalidate();
-    $request->session()->regenerateToken();
-
-    return redirect('/')->with('success', 'Your account has been deleted');
-}
+        return redirect('/')->with('success', 'Your account has been deleted');
+    }
 
 }
