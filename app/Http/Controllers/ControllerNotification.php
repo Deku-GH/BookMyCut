59117e7a8845542cc0520f70afac3ca74c37b417
mouad <mouadzed1@gmail.com>
2026-04-26 17:05:43 +0100

    first sdd the function of the index for affichage the notifation ' and the function of send the motification to tell tou if the booking confirmed or not

--- app/Http/Controllers/ControllerNotification.php
+++ app/Http/Controllers/ControllerNotification.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
+use App\Models\Notification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ControllerNotification extends Controller
 {
@@ -11,7 +14,45 @@ class ControllerNotification extends Controller
      */
     public function index()
     {
-        //
+        $notifications = Notification::where('user_id', auth()->id())
+            ->latest()
+            ->get();
+
+        return view('client.notifications', compact('notifications'));
+    }
+    public function send($id)
+    {
+        $booking = Booking::with('user')->find($id);
+        if ($booking->status == 'confirmed') {
+
+            Mail::raw(
+                'Your booking has been confirmed.',
+                function ($message) use ($booking) {
+
+                    $message->to($booking->user->email)
+                        ->subject('Booking Confirmed');
+                }
+
+            );
+
+            return redirect()->route('notification.store', [
+                'booking_id' => $booking->id
+            ]);
+
+
+        } elseif ($booking->status == 'cancelled') {
+
+            Mail::raw(
+                'Your booking has been cancelled.',
+                function ($message) use ($booking) {
+
+                    $message->to($booking->user->email)
+                        ->subject('Booking Cancelled');
+                }
+            );
+        }
+        return back()->with('success', 'Email sent');
+
     }
 
     /**
@@ -27,7 +68,26 @@ class ControllerNotification extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+
+        $booking = Booking::with('timeSlot')->findOrFail($request->booking_id);
+
+        $date = $booking->timeSlot->date;
+        $time = $booking->timeSlot->end_time;
+
+        $sendTime = date('Y-m-d H:i:s', strtotime($date . ' ' . $time . ' -15 minutes'));
+
+        Notification::create([
+            'user_id' => $booking->user_id,
+            'booking_id' => $booking->id,
+            'message' => 'Reminder: your booking is soon.',
+            'send_time' => $sendTime,
+            'is_sent' => false,
+        ]);
+
+        return redirect()->route('barber.dashboard')
+            ->with('success', 'Reminder created successfully');
+
     }
 
     /**
